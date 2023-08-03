6012c8f900fa51e28866e353c1d01b351e7cc092
Stanislav Leshuk <leshuksu@gmail.com>
2023-08-03 16:35:26 +0300

    played with enum

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { readFileSync } from "fs";
 const rawData = readFileSync("data/football.csv", { encoding: "utf-8" });
 const matches = rawData.split("\n").map((match) => match.split(","));
 
-const HOME_WIN = "H";
-const AWAY_WIN = "A";
+enum MATCH_RESULTS {
+  HOME_WIN = "H",
+  AWAY_WIN = "A",
+  DRAW = "D",
+}
 
 let manWins = 0;
 
 for (let match of matches) {
   if (
-    (match[1] === "Man United" && match[5] === HOME_WIN) ||
-    (match[2] === "Man United" && match[5] === AWAY_WIN)
+    (match[1] === "Man United" && match[5] === MATCH_RESULTS.HOME_WIN) ||
+    (match[2] === "Man United" && match[5] === MATCH_RESULTS.AWAY_WIN)
   ) {
     manWins++;
   }
