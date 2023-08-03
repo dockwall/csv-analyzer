57b13595bbd34d17d3234b1d1c607fd80539b35c
Stanislav Leshuk <leshuksu@gmail.com>
2023-08-03 16:23:00 +0300

    played with some bad analysis

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,18 @@ import { readFileSync } from "fs";
 const rawData = readFileSync("data/football.csv", { encoding: "utf-8" });
 const matches = rawData.split("\n").map((match) => match.split(","));
 
-console.log(matches);
+const HOME_WIN = "H";
+const AWAY_WIN = "A";
+
+let manWins = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === "Man United" && match[5] === HOME_WIN) ||
+    (match[2] === "Man United" && match[5] === AWAY_WIN)
+  ) {
+    manWins++;
+  }
+}
+
+console.log(manWins);
